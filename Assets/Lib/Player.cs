9867e7963eb36e2e74d4764352f6413040b6347d
Rob Whitehead <rob@improbable.io>
2023-07-01 15:59:41 +0100

    adding unit test gubbins

---
+++ Assets/Lib/Player.cs
@@ -0,0 +1,17 @@
+﻿namespace Lib
+{
+    public class Player {
+        public int Health { get; private set; }
+
+        public Player(int initialHealth) {
+            Health = initialHealth;
+        }
+
+        public void TakeDamage(int damage) {
+            Health -= damage;
+            if (Health < 0) {
+                Health = 0;
+            }
+        }
+    }
+}
---
+++ Assets/Lib/Tests/PlayerTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+namespace Lib.Tests
+{
+    public class PlayerTests {
+
+        // Each test will be a method marked with [Test]
+        [Test]
+        public void Player_TakeDamage_HealthDecreases() {
+            // Arrange
+            var player = new Player(10);  // Create a player with 10 health
+
+            // Act
+            player.TakeDamage(5);  // Player takes 5 damage
+
+            // Assert
+            Assert.AreEqual(5, player.Health);  // Player health should be 5
+        }
+
+        [Test]
+        public void Player_TakeDamage_HealthCannotGoBelowZero() {
+            // Arrange
+            var player = new Player(10);  // Create a player with 10 health
+
+            // Act
+            player.TakeDamage(15);  // Player takes 15 damage, more than total health
+
+            // Assert
+            Assert.AreEqual(0, player.Health);  // Player health should be 0, not negative
+        }
+    }
+}
